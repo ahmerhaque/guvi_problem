8dfbca68c4d21c44f86d6120d43b33adee7984a0
tafsirul haque <haquetafsirul@gmail.com>
2019-01-24 12:33:42 -0800

    guvi4.py

---
+++ guvi4.py
@@ -0,0 +1,22 @@
+def leastFrequent(arr, n):
+    arr.sort()
+    min_count = n + 1
+    res = -1
+    curr_count = 1
+    for i in range(1, n):
+        if (arr[i] == arr[i - 1]):
+            curr_count = curr_count + 1
+        else:
+            if (curr_count < min_count):
+                min_count = curr_count
+                res = arr[i - 1]
+            curr_count = 1
+    if (curr_count < min_count):
+        min_count = curr_count
+        res = arr[n - 1]
+    return res
+
+arr = list(map(int,input().split()))
+n = len(arr)
+print(leastFrequent(arr, n)) 
+
